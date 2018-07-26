7f41450e5e1b2adcbf20e96efd524777da7dba92
Sam Yang <samuel.q.yang@gmail.com>
2018-07-25 22:51:19 -0700

    final

---
+++ firebase.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyDqQoKK2MYCb_2FzUYmeafETdCVWi22JeI",
+    authDomain: "insta-tutors.firebaseapp.com",
+    databaseURL: "https://insta-tutors.firebaseio.com",
+    projectId: "insta-tutors",
+    storageBucket: "insta-tutors.appspot.com",
+    messagingSenderId: "624098741421"
+  };
+
+    firebase.initializeApp(config);
