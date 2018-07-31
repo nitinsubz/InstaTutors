a6a90cdbc9fc10f9c1fdb1ffd5cfa3af321366e2
Sam Yang <samuel.q.yang@gmail.com>
2018-07-31 00:12:54 -0700

    idk

---
+++ email.js
@@ -0,0 +1,34 @@
+console.log('* [example 1.1] sending test email');
+ 
+// Require'ing module and setting default options
+ 
+var send = require('gmail-send')({
+//var send = require('../index.js')({
+  user: 'instatutorsteam@gmail.com',
+  // user: credentials.user,                  // Your GMail account used to send emails
+  pass: 'instatuto3s',
+  // pass: credentials.pass,                  // Application-specific password
+  to:   'samuel.q.yang@gmail.com',
+  // to:   credentials.user,                  // Send to yourself
+                                           // you also may set array of recipients:
+                                           // [ 'user1@gmail.com', 'user2@gmail.com' ]
+  // from:    credentials.user,            // from: by default equals to user
+  // replyTo: credentials.user,            // replyTo: by default undefined
+  // bcc: 'some-user@mail.com',            // almost any option of `nodemailer` will be passed to it
+  subject: 'test subject',
+  text:    'gmail-send example 1',         // Plain text
+  //html:    '<b>html text</b>'            // HTML
+});
+ 
+ 
+// Override any default option and send email
+  console.log('* [example 1.1] sending test email');
+   
+  send({ // Overriding default parameters
+    subject: 'attached ',
+    text: 'tasjhasdg'         // Override value set as default
+  }, function (err, res) {
+    console.log('* [example 1.1] send() callback returned: err:', err, '; res:', res);
+  });
+
+ 
